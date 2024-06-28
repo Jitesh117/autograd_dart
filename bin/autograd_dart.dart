545113e7e8cd3f79c53f341ecfc773ac9d80b8d3
jitesh117 <sahookumarjitesh@gmail.com>
2024-06-28 15:26:04 +0530

    [chore]: readme

--- bin/autograd_dart.dart
+++ bin/autograd_dart.dart
@@ -1,5 +1,29 @@
-import 'package:autograd_dart/autograd_dart.dart' as autograd_dart;
+import 'package:autograd_dart/src/core/tensor.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${autograd_dart.calculate()}!');
+void main() {
+  // Create a 2x3 tensor filled with zeros
+  var t1 = Tensor.zeros([2, 3]);
+  print('t1: $t1');
+
+  // Create a 2x3 tensor filled with ones
+  var t2 = Tensor.ones([2, 3]);
+  print('t2: $t2');
+
+  // Create a 2x3 tensor with random values
+  var t3 = Tensor.random([2, 3]);
+  print('t3: $t3');
+
+  // Access and modify elements
+  print('t3[0, 1] before: ${t3[[0, 1]]}');
+  t3[[0, 1]] = 5.0;
+  print('t3[0, 1] after: ${t3[[0, 1]]}');
+
+  // Perform operations
+  var t4 = t2.add(t3);
+  print('t2 + t3: $t4');
+
+  var t5 = t2.multiply(t3);
+  print('t2 * t3: $t5');
+
+  // Try to add tensors with incompatible shapes
 }
