ae590644d67670e2aa163f0c7671188b1eb51a2d
jitesh117 <sahookumarjitesh@gmail.com>
2024-06-28 13:04:41 +0530

    init

---
+++ bin/autograd_dart.dart
@@ -0,0 +1,5 @@
+import 'package:autograd_dart/autograd_dart.dart' as autograd_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${autograd_dart.calculate()}!');
+}
---
+++ test/autograd_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:autograd_dart/autograd_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
